5e0a004e68296766fb18e02ebf4f3433b1987c1f
SergeyGalaxyOrsik <sergey.orsik.work@gmail.com>
2025-01-03 02:18:02 +0300

    second commit

---
+++ nglx-network/src/main/java/com/nglx/nglx/NglxNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.nglx.nglx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NglxNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NglxNetworkApiApplication.class, args);
+	}
+
+}
---
+++ nglx-network/src/test/java/com/nglx/nglx/NglxNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nglx.nglx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NglxNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
